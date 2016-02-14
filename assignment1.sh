204d913103b27a0e7e87574ebd732e7b4fad37c6
Hastings Greer <hastings.greer@gmail.com>
2016-02-14 15:17:53 -0500

    correct assingment script

--- assignment1.sh
+++ assignment1.sh
@@ -3,6 +3,7 @@ cd Release
 make clean
 make all
 cd ..
-Release/raytracer -f part1.ppm 
-Release/raytracer -f part2.ppm --shade
-Release/raytracer -f part3.ppm --shade --antialias
+Release/raytracer -o part1.ppm 
+Release/raytracer -o part2.ppm --shade
+Release/raytracer -o part3.ppm --shade --antialias
+Release/raytracer -o teapotproblems.ppm --shade --reflect --falloff --teapot
